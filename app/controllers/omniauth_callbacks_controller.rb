431ea972af68fb6e16093e65a23b37ba7214937d
Takamichi-tsutsumi <bdgaqh1889@gmail.com>
2015-10-18 15:13:12 +0900

    # 修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-	    sign_in_and_redirect @user
-	else
-		redirect_to :new_user_registration_path
-	end
+    sign_in_and_redirect @user
   end
 
   private
